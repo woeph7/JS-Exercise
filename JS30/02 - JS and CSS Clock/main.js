6fe371bdfd6e45e19a695fd9b3ee1c1f0ae7d546
woeph7 <aeishin0212@gmail.com>
2018-04-26 09:53:27 +0200

    Add files

---
+++ JS30/02 - JS and CSS Clock/main.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand   = document.querySelector('.hour-hand');
+
+function setDate(){
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360 ) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  //console.log(seconds);
+
+  const minutes = now.getMinutes();
+  const minutesDegrees =((minutes / 60) * 360) +90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hour = now.getMinutes();
+  const hourDegrees =((minutes / 12) * 360) +90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
