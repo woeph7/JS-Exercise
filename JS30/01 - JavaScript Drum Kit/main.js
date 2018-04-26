3dc4bf7d021a1a6dc0f143740ce7838d10575a45
woeph7 <aeishin0212@gmail.com>
2018-04-26 09:52:55 +0200

    Update Day1

--- JS30/01 - JavaScript Drum Kit/main.js
+++ JS30/01 - JavaScript Drum Kit/main.js
@@ -1,15 +1,17 @@
-window.addEventListener('keydown', function(event){
+function playSound(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if(!audio)ruturn; //stop running the  function
+  if(!audio)return; //stop running the  function
   audio.currentTime = 0; // rewind to start
   audio.play();
   key.classList.add('playing');
-});
+}
 
 function removeTransition(event){
   if(event.propertyName !== 'transform') return; //remove irrelevent informations
-  console.log(event.propertyName);
+  this.classList.remove('playing');
 }
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
