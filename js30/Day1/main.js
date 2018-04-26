124e36bda7bbc246fee6a29c72cfd61db7814439
woeph7 <aeishin0212@gmail.com>
2018-04-26 09:55:29 +0200

    Revert "Update Day1"
    This reverts commit 3dc4bf7d021a1a6dc0f143740ce7838d10575a45.

--- js30/Day1/main.js
+++ js30/Day1/main.js
@@ -1,17 +1,15 @@
-function playSound(event) {
+window.addEventListener('keydown', function(event){
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if(!audio)return; //stop running the  function
+  if(!audio)ruturn; //stop running the  function
   audio.currentTime = 0; // rewind to start
   audio.play();
   key.classList.add('playing');
-}
+});
 
 function removeTransition(event){
   if(event.propertyName !== 'transform') return; //remove irrelevent informations
-  this.classList.remove('playing');
+  console.log(event.propertyName);
 }
-
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
